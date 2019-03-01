9fdcfa0890674b8faea0b613c09c8eb856161e28
Mihailfusion <mihailfusion@gmail.com>
2019-03-01 21:57:11 +0300

    commit2

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 var money = prompt("Ваш бюджет на месяц?"); //бюджет
 var time = prompt("Введите дату в формате YYYY-MM-DD"); //дата
-var a = prompt("Введите обязательную статью расходов в этом месяце"),
-    b = prompt("Во сколько обойдется?");
+var a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    b1 = prompt("Во сколько обойдется?"),
+    a2 = prompt("Введите обязательную статью расходов в этом месяце"),
+    b2 = prompt("Во сколько обойдется?");
+
 
 
 
@@ -16,7 +19,8 @@ var appData = {
     savings: false
 };
 
-appData.expenses[a] = b;
+appData.expenses[a1] = b1;
+appData.expenses[a2] = b2;
 
 var result = appData["budget"] / 30;
 document.write("<h2> Ваш доход на один день состовляет = <u>" + result.toFixed(2) + "</u> </h2>");
